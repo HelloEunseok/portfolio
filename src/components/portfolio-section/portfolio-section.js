19c9c88dd9b41b879f316ea2ee15b145041ed814
EunSeok <eunseok@mirimmedialab.co.kr>
2019-01-15 17:30:21 +0900

    init project

---
+++ src/components/portfolio-section/portfolio-section.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './portfolio-section.scss';
+
+class PortfolioSection extends Component{
+    constructor(props){
+        super(props)
+    }
+    
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default PortfolioSection;
---
+++ src/components/resume-section/resume-section.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './resume-section.scss';
+
+class ResumeSection extends Component{
+    constructor(props){
+        super(props)
+    }
+    
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default ResumeSection;
